6a8304c68f5e981169f3d7e94693c1f5ae047f94
Ben Beya Samy <samyben2001@hotmail.com>
2022-12-20 16:58:54 +0100

    fix settings

--- snacksite/settings.py
+++ snacksite/settings.py
@@ -6,7 +6,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env(env_file=str(BASE_DIR / ".env"))
+environ.Env.read_env(env_file=str(BASE_DIR / "snacksite" / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
