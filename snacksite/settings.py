0e62ba9d063740ef1edd16ff737933cc7db6bc82
Ben Beya Samy <samyben2001@hotmail.com>
2022-12-21 19:00:06 +0100

    change settings for allowed host

--- snacksite/settings.py
+++ snacksite/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG")
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
