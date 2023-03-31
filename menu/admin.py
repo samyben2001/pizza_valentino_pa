150088ffedf11994c4c55e2427c0876b3ba31de0
Ben Beya Samy <samyben2001@hotmail.com>
2023-03-31 13:32:18 +0200

    fix horaire and annonce in DB

--- menu/admin.py
+++ menu/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.db import models
 from django.forms import CheckboxSelectMultiple
 from django.contrib.auth.models  import  Group 
-from .models import Ingredient,Allergene,Entree,Pizza,Pates,Dessert,Boisson
+from .models import Ingredient,Allergene,Entree,Pizza,Pates,Dessert,Boisson,Services,Horaire,Annonce
 
 admin.site.unregister(Group)  
 
@@ -45,6 +45,16 @@ class boissonAdmin(admin.ModelAdmin):
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
     }
 
+class servicesAdmin(admin.ModelAdmin):{}
+     
+
+class horaireAdmin(admin.ModelAdmin):
+        formfield_overrides = {
+        models.ManyToManyField: {'widget': CheckboxSelectMultiple},
+    }
+
+class annonceAdmin(admin.ModelAdmin):{}
+
 admin.site.register(Ingredient, ingredientAdmin)
 admin.site.register(Allergene, allergeneAdmin)
 admin.site.register(Entree, entreeAdmin)
@@ -52,3 +62,6 @@ admin.site.register(Pizza, pizzaAdmin)
 admin.site.register(Pates, patesAdmin)
 admin.site.register(Dessert, dessertsAdmin)
 admin.site.register(Boisson, boissonAdmin)
+admin.site.register(Services, servicesAdmin)
+admin.site.register(Horaire, horaireAdmin)
+admin.site.register(Annonce, annonceAdmin)
